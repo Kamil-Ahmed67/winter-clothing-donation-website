144737b9582c50315a9db5a9c10dbe38e98584d8
Kamil-Ahmed67 <nabilahmedtalukder6272@gmail.com>
2024-11-17 21:49:40 +0600

    Firebase Configuration Added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,11 @@
+import Navbar from "../components/Navbar/Navbar";
+
 const MainLayout = () => {
     return (
         <div>
-            
+            <header>
+                <Navbar></Navbar>
+            </header>
         </div>
     );
 };
